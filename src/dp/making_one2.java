aee3519c49cf3b69ca748fb8e3e0b30d17c70593
stevepark@samsung.com <steve.sanghee@gmail.com>
2021-05-12 07:50:03 -0400

    DP DP

--- src/dp/making_one2.java
+++ src/dp/making_one2.java
@@ -1,8 +1,6 @@
 package dp;
 
-
-//Logical Error... 자기것을 써야 되네.. d-1 이 아니라
-public class making_one {
+public class making_one2 {
 
 	static int cnt = 0;
 	static int d[];
@@ -19,35 +17,34 @@ public class making_one {
 		d[1] = 0;
 		
 		for (int i = 2; i <= N; i ++) {
-			
+
 			if (i == N) {
 				System.out.println("LMK");
 			}
-
-			if ( i % 5 == 0) {
-				d[i] = Math.min(d[i - 1], d[i / 5]) + 1; 
-			} 
 			
-			if ( i % 3 == 0) {
-				d[i] = Math.min(d[i - 1], d[i / 3]) + 1; 
-			}
+			
+			d[i] = d[i - 1] + 1;
 
 			if ( i % 2 == 0) {
-				d[i] = Math.min(d[i - 1], d[i / 2]) + 1; 
+				d[i] = Math.min(d[i], d[i / 2] + 1); 
 			}
 
-			if (( i % 5 != 0) &&  (i % 3 != 0) &&  (i % 2 != 0)) {
-				d[i] = d[i - 1] + 1;
+			if ( i % 3 == 0) {
+				d[i] = Math.min(d[i], d[i / 3] + 1); 
 			}
 			
+			if ( i % 5 == 0) {
+				d[i] = Math.min(d[i], d[i / 5] + 1); 
+			} 
+			
 		}
-		
-		
+
 		System.out.println(d[N]);
 		
 //		for (int i = 1; i < N+1; i ++) {
 //			System.out.println(d[i]);
 //		}
+		
 	}
 
 
